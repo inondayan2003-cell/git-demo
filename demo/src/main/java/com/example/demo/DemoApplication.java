e446a577c97e9f50e7221817f9a8c61a2ed65f0e
inondayan2003-cell <inondayan.2003@gmail.com>
2026-03-16 16:21:47 +0200

    comit from branch 2

--- demo/src/main/java/com/example/demo/DemoApplication.java
+++ demo/src/main/java/com/example/demo/DemoApplication.java
@@ -7,7 +7,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DemoApplication {
 
 	public static void main(String[] args) {
+		System.out.println("hello from branch");
 		SpringApplication.run(DemoApplication.class, args);
 	}
-	System.out.println("hello from branch");
+
 }
